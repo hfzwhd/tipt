9d8e4a3b1a02eb6207b474540a39ff871076c540
hfzwhd <hafizwahid.e@gmail.com>
2023-08-29 22:24:14 +0800

    update README.md

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ tipt is a python cli tool that allows you to get daily islamic prayer times in y
 
 only returns prayer times only for gombak, petaling, sepang, hulu langat, hulu selangor, s.alam.
 
+### screenshot
+
+![Screenshot from 2023-08-29 22-21-24.png](..%2F..%2FPictures%2FScreenshots%2FScreenshot%20from%202023-08-29%2022-21-24.png)
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import feedparser
 url = "https://www.e-solat.gov.my/index.php?r=esolatApi/xmlfeed&zon=SGR01"
 content = feedparser.parse(url)
 
-#print((content).keys())
-#print(content.entries)
-#print(content.entries[0].title + content.entries[0].summary)
-#print(content.feed)
+# print((content).keys())
+# print(content.entries)
+# print(content.entries[0].title + content.entries[0].summary)
+# print(content.feed)
 
 print("\n" + content.feed.updated + " | " + content.feed.subtitle + "\n")
 
@@ -16,4 +16,3 @@ for solat_schedule in content.entries:
     print(solat_schedule.title + ": " + solat_schedule.summary)
 
 print("\nProvider: " + content.feed.author)
-
